1371555943c16dd423b81ec57b38db97d7a4beeb
Giordani.dev <giordanisarturi@gmail.com>
2023-05-09 12:35:35 -0300

    renaming

--- newmain_wip.py
+++
@@ -1,8 +0,0 @@
-from app import App
-
-if __name__ == '__main__':
-    app = App()
-    app.geometry("500x300")
-    app.resizable(0, 0)
-    app.eval("tk::PlaceWindow . center")
-    app.mainloop()
