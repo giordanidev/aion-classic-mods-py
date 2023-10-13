23c5411f14f3e0a518b15f0d19db283295036d88
Giordani.dev <giordanisarturi@gmail.com>
2023-10-12 22:41:19 -0300

    fixed filters

--- tests/zip.py
+++ tests/zip.py
@@ -6,42 +6,44 @@ def main():
     file_name = '.\\tests\\aionfilter.zip'
     dest_path = '.\\download\\aionfilter.zip'
 
+    shutil.copy2('.\\download\\aionfilterchat.dat', 'aionfilterchat.dat')
     # Opening the 'Zip' in writing mode
-    with zipfile.ZipFile('.\\tests\\aionfilterchat_load.zip', 'w') as file:
+    with zipfile.ZipFile('aionfilterchat_load.zip', 'w') as file:
         # write mode overrides all the existing files in the 'Zip.'
         # you have to create the file which you have to write to the 'Zip.'
-        file.write('.\\download\\aionfilterchat.dat')
+        file.write('aionfilterchat.dat')
         print('Novo arquivo "aionfilterchat_load.zip" criado.')
 
     # opening the 'Zip' in reading mode to check
-    with zipfile.ZipFile('.\\tests\\aionfilterchat_load.zip', 'r') as file:
+    with zipfile.ZipFile('aionfilterchat_load.zip', 'r') as file:
         print(file.namelist())
     
-    os.rename('.\\tests\\aionfilterchat_load.zip', '.\\tests\\aionfilterchat_load.pak')
+    os.rename('aionfilterchat_load.zip', 'aionfilterchat_load.pak')
     print('Arquivo "aionfilterchat_load.pak" renomeado.')
 
-        # Opening the 'Zip' in writing mode
-    with zipfile.ZipFile('.\\tests\\aionfilterline_load.zip', 'w') as file:
+    shutil.copy2('.\\download\\aionfilterline.dat', 'aionfilterline.dat')
+    # Opening the 'Zip' in writing mode
+    with zipfile.ZipFile('aionfilterline_load.zip', 'w') as file:
         # write mode overrides all the existing files in the 'Zip.'
         # you have to create the file which you have to write to the 'Zip.'
-        file.write('.\\download\\aionfilterline.dat')
+        file.write('aionfilterline.dat')
         print('Novo arquivo "aionfilterline_load.zip" criado.')
 
     # opening the 'Zip' in reading mode to check
-    with zipfile.ZipFile('.\\tests\\aionfilterline_load.zip', 'r') as file:
+    with zipfile.ZipFile('aionfilterline_load.zip', 'r') as file:
         print(file.namelist())
 
-    os.rename('.\\tests\\aionfilterline_load.zip', '.\\tests\\aionfilterline_load.pak')
+    os.rename('aionfilterline_load.zip', 'aionfilterline_load.pak')
     print('Arquivo "aionfilterline_load.pak" renomeado.')
 
     # Opening the 'Zip' in writing mode
     with zipfile.ZipFile(file_name, 'w') as file:
         # write mode overrides all the existing files in the 'Zip.'
         # you have to create the file which you have to write to the 'Zip.'
-        file.write('.\\tests\\aionfilterchat_load.pak')
-        file.write('.\\tests\\aionfilterline_load.pak')
-        file.write('.\\download\\aionfilterchat.dat')
-        file.write('.\\download\\aionfilterline.dat')
+        file.write('aionfilterchat_load.pak')
+        file.write('aionfilterline_load.pak')
+        file.write('aionfilterchat.dat')
+        file.write('aionfilterline.dat')
         print('Novo zip criado.')
 
     # opening the 'Zip' in reading mode to check
@@ -52,8 +54,10 @@ def main():
     print('Arquivo temporário copiado para a pasta "download".')
 
     os.remove(file_name)
-    os.remove('.\\tests\\aionfilterchat_load.pak')
-    os.remove('.\\tests\\aionfilterline_load.pak')
+    os.remove('aionfilterchat_load.pak')
+    os.remove('aionfilterline_load.pak')
+    os.remove('aionfilterchat.dat')    
+    os.remove('aionfilterline.dat')
     print('Arquivos temporários removidos.')
 
 if __name__ == '__main__': main()
