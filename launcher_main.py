ebd192ef9ffbb8a9991ebcca442741309b126ae7
Giordani.dev <giordanisarturi@gmail.com>
2023-12-04 13:47:10 -0300

    starting to implement launcher/updater

---
+++ launcher_main.py
@@ -0,0 +1,26 @@
+#TODO LAUNCHER TO CHECK PROGRAM UPDATES
+#DOWNLOAD AND INSTALL UPDATE? YES/NO.
+
+from functions import *
+import tkinter as tk, customtkinter as ctk, logging
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title(translateText("app_title") + translateText("app_launcher_title") + translateText("app_launcher_version"))
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        # System appearance
+        ctk.set_appearance_mode(app_config.get('app', 'theme'))
+        ctk.set_default_color_theme(app_config.get('app', 'color').lower())
+
+
+app = App()
+app.iconbitmap("./config/img/AionClassicMods.ico")
+app.geometry("450x150")
+app.resizable(0, 0)
+app.eval("tk::PlaceWindow . center")
+app.mainloop()
