ef6ee85ea88b482b307c917c6a46a10cf2406289
Giordani.dev <giordanisarturi@gmail.com>
2023-12-09 04:09:15 -0300

    progress barr

--- main_updater.py
+++ main_updater.py
@@ -12,27 +12,54 @@ class App(ctk.CTk):
 
         global app_config
 
-        self.title(translateText("app_title") + translateText("app_updater_title") + translateText("app_updater_version"))
-
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(0, weight=1)
-
-        # System appearance
-        ctk.set_appearance_mode(app_config['theme'])
-        ctk.set_default_color_theme(app_config['color'].lower())
-
-        #center app on main screen 
-        centerApp(450, 150, self)
-
         if app_config['on_start'] == True:
 
-            self.updaterFrame = ctk.CTkFrame(self)
-            self.updaterFrame.grid(row=0, column=0, sticky="new")
-            self.updaterFrame.configure(fg_color="transparent")
-            self.updaterFrame.grid_columnconfigure(1, weight=1)
+            self.title(translateText("app_title") + translateText("app_updater_title") + translateText("app_updater_version"))
 
-            self.infoLabel = ctk.CTkLabel(self.updaterFrame, text="Aguardando")
-            self.infoLabel.grid(row=0, column=0, columnspan=2, padx=padx_both, pady=pady_both, sticky="we")
+            self.grid_rowconfigure(0, weight=1)
+            self.grid_columnconfigure(0, weight=1)
+
+            # System appearance
+            ctk.set_appearance_mode(app_config['theme'])
+            ctk.set_default_color_theme(app_config['color'].lower())
+
+            #center app on main screen 
+            centerApp(450, 150, self)
+
+            self.updaterFrame = ctk.CTkFrame(self)
+            self.updaterFrame.grid(row=0, column=0, sticky="ew")
+            self.updaterFrame.configure(fg_color="transparent")
+            self.updaterFrame.grid_columnconfigure(0, weight=1)
+            self.updaterFrame.grid_rowconfigure(3, weight=1)
+
+            self.infoLabel = ctk.CTkLabel(self.updaterFrame, text=translateText("app_updater_searching"))
+            self.infoLabel.grid(row=0, column=0, columnspan=4, padx=padx_both, pady=pady_both)
+
+            self.installButton = ctk.CTkButton(self.updaterFrame, text=translateText("app_button_update"), state="disabled", width=90)
+            self.installButton.grid(row=1, column=1, padx=padx_both, pady=pady_both, sticky="ew")
+
+            self.ignoreButton = ctk.CTkButton(self.updaterFrame, text=translateText("app_button_updater_ignore"), state="disabled", width=90)
+            self.ignoreButton.grid(row=1, column=2, padx=padx_both, pady=pady_both, sticky="ew")
+            
+            self.progressbar = ctk.CTkProgressBar(self.updaterFrame, orientation="horizontal", width=280, mode="indeterminate")
+            self.progressbar.grid(row=2, column=0, columnspan=4, padx=padx_both, pady=pady_both)
+            self.progressbar.start()
+
+            def checkbox_event():
+                print("checkbox toggled, current value:", self.on_startCheckbox.get())
+                
+                if self.on_startCheckbox.get() == False:
+                    self.on_startCheckbox.deselect()
+                    self.progressbar.stop()
+                    self.progressbar.configure(mode="determinate")
+                    self.progressbar.set(0)
+                else:
+                    self.on_startCheckbox.select()
+                    self.progressbar.configure(mode="indeterminate")
+                    self.progressbar.start()
+            self.on_startCheckbox = ctk.CTkCheckBox(self.updaterFrame, text=translateText("app_updater_on_start"), command=checkbox_event, onvalue=True, offvalue=False)
+            self.on_startCheckbox.grid(row=3, column=3, padx=padx_both, pady=pady_both, sticky="s")
+            self.on_startCheckbox.select()
             
             cloud_version = checkUpdates()
             
